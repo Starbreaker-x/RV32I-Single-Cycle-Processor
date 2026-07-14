d1c41efad5a5bbba4d46dea9bc0683a11fd786b0
Starbreaker-x <donike1606@gmail.com>
2026-07-13 22:40:15 -0400

    Started the test bench

---
+++ tb/RegisterFile_TestBench.sv
@@ -0,0 +1,23 @@
+//Test Cases
+/*   input into the register and make sure you read the correct value (for both registers pointed at by A1 & A2)
+     Write input into register and see if the register changes values on the rising clock edge
+     Read previous value then write and see if the new value read is correct
+     Make sure that writing only occurs on the rising edge of a clock, and when Write Enable is high( so 4 seperate cases fo this)
+     Make sure that register x0 reads as 32'd0 and cannot be written to always
+
+        
+     */
+
+module RF_tb(){
+
+input logic [4:0] A1, A2, A3, 
+input logic [31:0] WD3, 
+input logic WE3, clk,
+output logic [31:0] RD1, RD2
+
+
+Reg_File dut(A1, A2, A3, WD3 , WE3 , clk , RD1 , RD2)
+
+//I'll finish up  in the morning
+
+}
