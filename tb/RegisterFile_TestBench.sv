69e59e338c7c3f2d0f2e5999c7c9029b4de794ea
Starbreaker-x <donike1606@gmail.com>
2026-07-14 23:16:46 -0400

    Got an assertion violation in Vivado, will andd $error displays tomorrow. time for bed

--- tb/RegisterFile_TestBench.sv
+++ tb/RegisterFile_TestBench.sv
@@ -12,10 +12,86 @@ module RF_tb();
 
 logic [4:0] A1, A2, A3;
 logic [31:0] WD3;
-logic WE3, clk;
+logic WE3, clk = 0;
 logic [31:0] RD1, RD2;
 
 
 Reg_File dut(A1, A2, A3, WD3 , WE3 , clk , RD1 , RD2);
+
+always #5 clk = ~clk;
+
+initial begin 
+
+
+//writing into register and reading from RD1
+@(negedge clk);
+WD3 = 32'b11110000111100001111000011110000; A3 = 5'b00001; A1 = 5'b00001; WE3 = 1;
+@(posedge clk);
+#1;
+assert( RD1 === 32'b11110000111100001111000011110000 );
+
+//writing into register and reading from RD2
+@(negedge clk);
+WD3 = 32'b00001111000011110000111100001111; A3 = 5'b00011; A2 = 5'b00011;
+@(posedge clk);
+#1;
+assert( RD2 === 32'b00001111000011110000111100001111);
+
+/*Seeing if we try writing into a register but WE3 is low so nothing changes for bot Reading Registers*/
+
+//RD1 check
+@(negedge clk);
+WD3 = 32'b00000000000000000000000000000001; A3 = 5'b00001; A1 = 5'b00001; WE3 = 0;
+@(posedge clk);
+#1;
+assert( RD1 === 32'b11110000111100001111000011110000 );
+
+
+//RD2 check
+@(negedge clk);
+A3 = 5'b00011; A2 = 5'b00011;
+@(posedge clk);
+#1
+assert( RD2 === 32'b00001111000011110000111100001111 );
+
+/*Checking to see if register reads the correct value before and after a new write*/
+
+//RD1
+
+@(negedge clk);
+assert( RD1 === 32'b11110000111100001111000011110000 ); A3 = 5'b00001; WE3 = 1;
+@(posedge clk);
+#1;
+assert( RD1 === 32'b00000000000000000000000000000001 ); 
+
+//RD2
+
+@(negedge clk);
+assert( RD2 ===  32'b000011110000111100001111000011110); A3 = 5'b00011;
+@(posedge clk);
+#1;
+assert( RD2 ===  32'b00000000000000000000000000000001 );
+
+/* x0 checks, must be read as 0 and will be 0 no matter what is weritten to it*/
+
+
+//Reading check
+@(negedge clk);
+A1 = 5'b00000; A2 = 5'b00000;
+@(posedge clk);
+#1;
+assert( (RD1 === 32'd0) && (RD2 === 32'd0) );
+
+//Writing check
+@(negedge clk);
+A3 = 5'b00000;
+@(posedge clk);
+#1
+assert( (RD1 === 32'd0) && (RD2 === 32'd0) );
+
+
+
+
+end
 
 endmodule
