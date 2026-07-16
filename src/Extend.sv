43b8f67aec9d0dcf9961a69c945163ca84fa7949
Starbreaker-x <donike1606@gmail.com>
2026-07-16 10:07:21 -0400

    Forgot to add a defualt case and used assing in always comb. Still learning syntax

--- src/Extend.sv
+++ src/Extend.sv
@@ -12,9 +12,10 @@ module Extend( input logic [31:0] Instr, input logic [1:0] ImmSrc, output logic 
 
         case(ImmSrc) 
 
-        2'b00: assign ImmExt = {{20{Instr[31]}},Instr[31:20]}; // I Type
-        2'b01: assign ImmExt = {{20{Instr[31]}},Instr[31:25],Instr[11:7]}; // S Type
-        2'b10: assign ImmExt = {{20{Instr[31]}},Instr[7],Instr[30:25],Instr[11:8],1'b0};
+        2'b00:  ImmExt = {{20{Instr[31]}},Instr[31:20]}; // I Type
+        2'b01:  ImmExt = {{20{Instr[31]}},Instr[31:25],Instr[11:7]}; // S Type
+        2'b10:  ImmExt = {{20{Instr[31]}},Instr[7],Instr[30:25],Instr[11:8],1'b0};
+        default:ImmExt = 32'bx;// undefined output
 
         
         endcase
