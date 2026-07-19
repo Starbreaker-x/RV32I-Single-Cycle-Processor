a46067256084de00bd4683ad546cc65466683d57
Starbreaker-x <donike1606@gmail.com>
2026-07-18 21:17:47 -0400

    So, I guess Vivado doesnt support .hex files so I can't view what I put into the file there and I made some syntax errors again

--- src/Instruction_Memory.sv
+++ src/Instruction_Memory.sv
@@ -1,9 +1,9 @@
-module IM( input [31:0] logic A, output [31:0] logic RD);
+module IM( input logic [31:0] A, output logic [31:0] RD);
 
 
 //Chose to do by byte addressing 
 logic [7:0] mem [0:1023];
-
+ 
 
 //initializing our program
 initial begin 
@@ -11,7 +11,7 @@ initial begin
 end
 
 //Little Endian for RISC-V
-assign = {mem[A + 3], mem[A + 2], mem[A + 1], mem[A]};
+assign RD = {mem[A + 3], mem[A + 2], mem[A + 1], mem[A]};
 
 
 endmodule
