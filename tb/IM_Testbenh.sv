a006910fd438d96972845beed2484a7e1164ba64
Starbreaker-x <donike1606@gmail.com>
2026-07-18 23:02:24 -0400

    Yeah so I ordered my hex file in the wrong endian order so the behavioral simulation I ran failed assertions, also my second assertions output was undefined so I'm going to see why thats the case

---
+++ tb/IM_Testbenh.sv
@@ -0,0 +1,37 @@
+module IM_tb();
+
+logic [31:0] A, RD;
+
+IM dut(A , RD);
+
+
+task check( input logic [31:0] expected, input string testname);
+
+if(RD == expected)
+    $display("At address %h is: %b, %s passed", A, RD, testname);
+else
+$display(" %s failed: At address %h is: %b, expected: %b ", testname, A, RD, expected);
+endtask
+
+
+
+
+
+initial begin
+
+A = 32'h0000_0000;
+#1
+assert(RD === 32'hFFC4A303);
+
+A = 32'h0000_0100;
+#1
+assert(RD === 32'h0064A423);
+
+
+
+
+
+end
+
+
+endmodule
