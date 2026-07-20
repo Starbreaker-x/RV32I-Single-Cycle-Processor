8673be31efd941bed33098fd8ae79171230c067d
Starbreaker-x <donike1606@gmail.com>
2026-07-20 14:28:59 -0400

    PC tettsbench skeleton

---
+++ tb/PC_TestBench.sv
@@ -0,0 +1,15 @@
+/* Time to create the testbench for the Program Counter(PC), I've never made a testbench on modules 
+that are technically made up of other modules, so lets see how I do*/
+
+module PC_tb();
+
+
+
+
+
+
+
+
+
+
+endmodule;
