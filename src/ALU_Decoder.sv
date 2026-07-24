50067c51659de93a4cb44b0861f286a2a31adeb1
Starbreaker-x <donike1606@gmail.com>
2026-07-23 23:01:21 -0400

    I found a table in the front of the textbook that contains the rest of the instructions I need to implement.

--- src/ALU_Decoder.sv
+++ src/ALU_Decoder.sv
@@ -3,55 +3,37 @@ input logic [1:0] ALUOp, input logic [2:0] funct3, output logic ALU_Control);
 
 case(ALUOp)
 
-00: ALU_Control = 3'b000;
+00: ALU_Control = 3'b000; // lw/sw
 
-01: ALU_Control = 3'b001;
-
-
-10:  begin
+01: ALU_Control = 3'b001; // beq
 
 
+10:  begin  // ALUOp 10 dictates an R-type instruction
 
+case(funct3)
 
+000: begin
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
-
-
-
-
-
+if( Op && funct7){
+    ALU_Control = 3'b001; //sub
+}else{
+    ALU_Control = 3'b000; //add
+}
 
 end
+
+010: ALU_Control = 3'b101; //slt
+
+110: ALU_Control = 3'b011; //or
+
+111: ALU_Control = 3'b010; //and
+
+// Still need SLTU , XOR, SLL, SRL, SRA
 
 
 endcase
 
-
-
-
-
-
-
-
-
-
-
-
-
+end
 
 
 endmodule
