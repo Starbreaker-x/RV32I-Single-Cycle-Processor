00927c644c90efe953e103109a900af86c60809d
Starbreaker-x <donike1606@gmail.com>
2026-07-31 01:41:58 -0400

    Modifying the top module testBench to display values from the Registers in the register file

--- tb/TopModule_TestBench.sv
+++ tb/TopModule_TestBench.sv
@@ -9,6 +9,19 @@ RV32I_SC dut( clk , reset , PC);
 
 always #5 clk = ~clk;
 
+task check( input logic [31:0] expected , input logic [31:0] regnum,  input string instruction_name );
+
+if( dut.RegisterFile.registers[regnum] === expected )
+$display("Register %d contains the value: %d , %s instruction passed", regnum, expected , instruction_name );
+else
+$display("Register %d contains the value: %d , expected: %d , %s instruction failed", regnum , dut.RegisterFile.registers[regnum] , expected , instruction_name );
+
+endtask
+
+
+
+
+
 initial begin
 
 reset = 1'b1;
@@ -18,7 +31,7 @@ reset = 1'b0;
 
 
 @(posedge clk);
-$display("PC: %%h", PC);
+$display("PC: %h", PC); 
 
 
 end
