8a85646ad5156c21719d2d94d572609c03e249ab
Starbreaker-x <donike1606@gmail.com>
2026-08-17 01:56:50 -0400

    Decided not to use the MemWidth control signal and instead encode everytihing in the Register File and DataMemory

--- src/Register_File.sv
+++ src/Register_File.sv
@@ -1,6 +1,8 @@
 module Reg_File(input logic [4:0] A1, A2, A3, 
 input logic [31:0] WD3, 
 input logic WE3, clk,
+input logic [6:0] Op,
+input logic [2:0] funct3,
 output logic [31:0] RD1, RD2);
 
 /*  A1 and A2 are the registers we want to read from
@@ -18,8 +20,32 @@ output logic [31:0] RD1, RD2);
 
             always_ff @ ( posedge clk ) begin
 
-                if((A3 && WE3) != 5'd0)
-                  Registers[A3] <= WD3;
+                if((A3 && WE3) != 5'd0) begin
+                
+                    if(Op == 7'd3) begin
+                     
+                        case( funct3 )
+
+
+                            3'b000: Registers[A3] <= { 24{WD3[7]} , WD3[7:0] }; // lb
+
+                            3'b001: Registers[A3] <= { 16{WD3[15]} , WD3[15:0] }; // lh
+
+                            3'b010: Registers[A3] <= WD3; // lw
+
+                            3'b100: Registers[A3] <= { 24{1'b0} , WD3[7:0] }; // lbu
+
+                            3'b101: Registers[A3] <= { 16{1'b0} , WD3[15:0] }; // lhu
+
+
+                        endcase
+
+                    
+                    end
+                    else
+                    Registers[A3] <= WD3;
+
+                end
 
             end
 
