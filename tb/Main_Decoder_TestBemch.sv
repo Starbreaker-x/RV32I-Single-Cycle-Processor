28aa717f8d5e4f740b9ede38b1cec7109a7889ee
Starbreaker-x <donike1606@gmail.com>
2026-08-21 03:05:47 -0400

    Added cases for jal and jalr to the Main Decoder testbench

--- tb/Main_Decoder_TestBemch.sv
+++ tb/Main_Decoder_TestBemch.sv
@@ -69,33 +69,43 @@ check( 7'b0000011 , 1'b1 , 1'b1 , 1'b0 , 1'b1 , 1'b0 , 3'b000 , 2'b00 , "lw" );
 
 Op = 7'b0100011;// S-type
 #5;
-check( 7'b0100011 , 1'b0 , 1'b1 , 1'b1 , 1'bx , 1'b0 , 3'b001 , 2'b00 , "S-type" );
+check( 7'b0100011 , 1'b0 , 1'b1 , 1'b1 , 3'bxxx , 1'b0 , 3'b001 , 2'b00 , "S-type" );
 
 Op = 7'b0110011;//R-type
 #5;
-check( 7'b0110011 , 1'b1 , 1'b0 , 1'b0 , 1'b0 , 1'b0 , 3'bxxx , 2'b10 , "R-type");
+check( 7'b0110011 , 1'b1 , 1'b0 , 1'b0 , 3'b000 , 1'b0 , 3'bxxx , 2'b10 , "R-type");
 
 Op = 7'b1100011;//B-type
 #5;
-check( 7'b1100011 , 1'b0 , 1'b0 , 1'b0 , 1'bx , 1'b1 , 3'b010 , 2'b01 , "B-Type");
+check( 7'b1100011 , 1'b0 , 1'b0 , 1'b0 , 3'bxxx , 1'b1 , 3'b010 , 2'b01 , "B-Type");
 
 Op = 7'b00010011;//I-type immediate instructions
 #5;
-check( 7'b00010011 , 1'b1 , 1'b1, 1'b0 , 1'b0 , 1'b0 , 3'b000 , 2'b10 , "I-type immediate");
+check( 7'b00010011 , 1'b1 , 1'b1, 1'b0 , 3'b000 , 1'b0 , 3'b000 , 2'b10 , "I-type immediate");
 
 //U-type instructions
 
 Op = 7'b0110111; // lui
 #5;
-check( 7'b0110111 , 1'b1 , 1'b1 , 1'b0 , 3'b001 , 1'b0 , 3'b100 , 2'bxx , "lui" );
+check( 7'b0110111 , 1'b1 , 1'b1 , 1'b0 , 3'b011 , 1'b0 , 3'b100 , 2'bxx , "lui" );
 
 Op = 7'b0010111; //auipc
 #5;
 check( 7'b0110111 , 1'b1 , 1'b1 , 1'b0 , 3'b100 , 1'b0 , 3'b100 , 2'bxx , "auipc" );
 
+// J-type jal and I-type jalr
+
+Op = 7'b1101111; //jal
+#5;
+check( 7'b1101111 , 1'b1 , 1'b1 , 1'b0 , 3'b010 , 1'b1 , 3'b011 , 2'bxx , "jal" );
+
+Op = 7'b1100111; //jalr
+#5;
+check( 7'b1100111 , 1'b1 , 1'b1 , 1'b0 , 3'b010 , 1'b1 , 3'b000 , 2'bxx , "jalr");
+
 Op = 7'b1111111;//default test code
 #5;
-check( 7'b1111111 , 1'bx, 1'bx , 1'bx , 1'bx , 1'bx , 3'bxxx , 2'bxx , "default");
+check( 7'b1111111 , 1'bx, 1'bx , 1'bx , 3'bxxx , 1'bx , 3'bxxx , 2'bxx , "default");
 $display("All test passed");
 $finish;
 
