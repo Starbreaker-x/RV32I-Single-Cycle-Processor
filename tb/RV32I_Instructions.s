f7f9c3e0276252098a618550056a2f5a0d30f566
Starbreaker-x <donike1606@gmail.com>
2026-08-22 15:51:40 -0400

    Updating Instuction Memory initialization file

--- tb/RV32I_Instructions.s
+++ tb/RV32I_Instructions.s
@@ -1,32 +1,33 @@
-# 4 at address 0
-lw x1, 0(x0);
-#15 at address 4
-lw x2, 4(x0);
-#8 at address 8
-lw x3, 8(x0);
-#2 at address 12
-lw x4, 12(x0);
-# 4,026,531,840 at address 16
-lw x5, 16(x0);
-# 7 at address 20
-lw x6, 20(x0);
+# 4 at reg x1
+addi x1, x0, 4;
+#15 at reg x2
+addi x2, x0, 15;
+#8 at  reg x3
+addi x3, x0, 8;
+#2 at reg x4
+addi x4, x0, 2;
+# 4,026,531,840 at reg x5
+lui x5, 0xF0000;
+ori x5, x5 , 0x000;
+# 7 at reg x6
+addi x6, x0, 7;
 
 #R type instructions
 
-#ADD
+#add
 add x7, x1, x2;
-#SUB
+#sub
 sub x8, x2, x1;
-#AND
+#and
 and x9, x2, x6;
-#OR
+#or
 or x10, x1, x4;
-#XOR
+#xor
 xor x11, x2, x6;
-#SLT
+#slt
 slt x12, x5, x4;
 slt x13, x4, x5;
-#SLTU
+#sltu
 sltu x14, x5, x4;
 sltu x15, x4, x5;
 #SLL
@@ -47,6 +48,134 @@ sra x19, x5, x4;
 sra x20, x3, x4;
 
 #end of R type instructions
+
+#Immediate instructions
+
+
+
+#slti
+slti x21, x4, -15;
+
+slti x21, x4, 1;
+
+#sltiu
+sltiu x22, x4, -15;
+
+sltiu x22, x4, 0;
+
+#xori
+
+#slli
+
+#srli
+
+#srai
+
+#andi
+
+#jalr
+
+
+#end of Immediate instructions
+
+
+#auipc
+
+
+#jal
+
+#S-type instructions
+
+
+
+
+
+
+
+#end of S-type instructions
+
+
+# Load instructions
+
+
+#lb
+
+#lh
+
+#lbu
+
+#lhu
+
+#end of Load instructions
+
+
+# B-type instructions
+
+#beq
+
+
+
+#beq
+
+
+#bne
+
+
+#bne
+
+
+#blt
+
+
+#blt
+
+
+#bge
+
+
+#bge
+
+
+#bltu
+
+
+#bltu
+
+
+#bgeu
+
+
+#bgeu
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# end of B-type instructions
+
+
+
+
+
+
+
+
+
+
+
 
 #sw S Type
 
